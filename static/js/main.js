9e152690d64d58cf9ffc66510eca1c19aee6f1a7
deepblack <jonathan.vincent2@usherbrooke.ca>
2017-10-10 12:18:30 -0400

    Add local and external site utilisation

--- static/js/main.js
+++ static/js/main.js
@@ -11,15 +11,13 @@ function(indexPage, editPage, colormap, util) {
   // Create a colormap for display. The following is an example.
   function createColormap(label, labels) {
     return (label) ?
-      colormap.create("single", {
+      colormap.create("hsv", {
         size: labels.length,
         index: labels.indexOf(label)
       }) :
-      [[255, 255, 255],
-       [200, 0, 0],
-       [64, 32, 32]].concat(colormap.create("hsv", {
-        size: labels.length - 3
-      }));
+      colormap.create("hsv", {
+        size: labels.length
+      });
   }
 
   // Load dataset before rendering a view.
